e5e787c7a56b72b047bd8f02009898aa07e6b64d
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-05-11 18:52:18 -0500

    Estructura básica y bacia para comensar la API

--- db/resolve.js
+++ db/resolve.js
@@ -3,10 +3,7 @@ const courses  = require('./db');
 // Resolvers
 const resolvers = {
   Query: {
-    getCourses: (_, { input }, ctx) => {
-      const result = courses.filter(course => course.technology === input.technology);
-      return result;
-    }
+    
   }
 }
 
--- db/schema.js
+++ db/schema.js
@@ -2,22 +2,6 @@ const { gql } = require('apollo-server');
 
 // Schema
 const typeDefs = gql`
-  
-  type Course {
-    title: String
-  }
-  type Technology {
-    technology: String
-  }
-
-  input CourseInput {
-    technology: String
-  }
-
-  type Query {
-    getCourses(input: CourseInput!): [Course]
-    getTechnologys: [Technology]
-  }
 
 `;
 
