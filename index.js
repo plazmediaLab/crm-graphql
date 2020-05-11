1b6d38d6eba97d62df5de7358c0e93c65ca46caa
Adrian Nieves Uribe <contacto.plazmedia@gmail.com>
2020-05-11 16:03:43 -0500

    Primer Schema en Apollo serever y GraphQL

---
+++ index.js
@@ -0,0 +1,52 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Schema
+const typeDefs = gql`
+  
+  type Course {
+    title: String
+    technology: String
+
+  }
+
+  type Query {
+    getCourses: Course
+  }
+`;
+
+const courses = [
+  {
+      title: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+      technology: 'JavaScript ES6',
+  },
+  {
+      title: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+      technology: 'React',
+  },
+  {
+      title: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+      technology: 'Node.js'
+  }, 
+  {
+      title: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+      technology: 'React'
+  }
+];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    getCourses: () => courses[0]
+  }
+}
+
+// Server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// Run server
+server.listen().then( ({ url }) => {
+  console.log(`Server run on URL: ${url}`);
+} );
